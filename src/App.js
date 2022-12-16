3adbde1ad66eb19b7fae49f79c857870fa791855
muhammadtalha1023 <mohammadtalha1023.com>
2022-12-16 17:03:25 +0500

    React App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import Domestic from './Components/Domestic';
-// import International from './Components/International';
-// import Loginform from './Components/Loginform';
-// import Signupform from './Components/Signupform';
-// import {Route, Routes} from 'react-router-dom';
-// import Home from'./Components/Home';
-// import BankStatementletter from './Components/BankStatementletter';
-// import CoverStatementletter from './Components/CoverStatementletter';
-import Game from './Components/Game';
+import Domestic from './Components/Domestic';
+import International from './Components/International';
+import Loginform from './Components/Loginform';
+import Signupform from './Components/Signupform';
+import {Route, Routes} from 'react-router-dom';
+import Home from'./Components/Home';
+import BankStatementletter from './Components/BankStatementletter';
+import CoverStatementletter from './Components/CoverStatementletter';
+// import Game from './Components/Game';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route path='/' element={<Home/>}/>
+      <Routes>
+        <Route path='/Home' element={<Home/>}/>
         <Route path='/Home' element={<Home/>}/>
         <Route path='/BankStatementletter' element={<BankStatementletter/>}/>
         <Route path='/CoverStatementletter' element={<CoverStatementletter/>}/>
@@ -22,8 +22,8 @@ function App() {
         <Route path='/Signupform' element={<Signupform/>}/>
         <Route path='/Domestic' element={<Domestic/>}/>
         <Route path='International' element={<International/>}/>
-      </Routes> */}
-      <Game/>
+      </Routes>
+      {/* <Game/> */}
  
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-import pic from "../Images/logo.png"
+import pic from "../Images/Logo.png"
 import './Navbar.css';
 
 
