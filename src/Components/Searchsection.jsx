2d2beb0d535f79002b517d0f462990eb6f569d8c
muhammadtalha1023 <mohammadtalha1023.com>
2022-12-21 19:55:07 +0500

    react

--- src/Components/Searchsection.jsx
+++ src/Components/Searchsection.jsx
@@ -14,13 +14,26 @@ function Searchsection() {
   const handleShow = () => setShow(true);
 
   const[value,setValue]=useState(0);
+  const[ num, setnum] = useState('')
   const inc=()=>{
-    setValue (value + 1);
+    if(value < 6){
+      setValue (value + 1);
+    }else{
+      setValue(6)
+    } 
+    }
 
-  }
   const dec=()=>{
-    setValue (value - 1);
+    if(value < 1){
+      setValue (0);
+    }else{
+      setValue(value-1)
+    }
   }
+
+  const apply = ()=>{
+    setnum(value)
+}
 
 
   return (
@@ -44,19 +57,19 @@ function Searchsection() {
         <input type="date" className='d1'/>
         </Col>
 
-        <Col className='s3col  bg-light pt-4'lg={2} xs={12}>
+        <Col className='s2col  bg-light pt-4'lg={2} xs={12}>
         <p>RETURN</p>
         <input type="date" className='d2'/>
         </Col>
 
-        <Col className='s4col  bg-light'lg={2} xs={12}>
+        <Col className='s2col  bg-light'lg={2} xs={12}>
         <p>TRAVELERS</p>
 
         <Button className='btntra' onClick={handleShow}>
-        Traveler
+        {num} {handleClose} Traveler
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal  show={show} onHide={handleClose}>
       <Row>
         <Col className='coltra'>
         <h4>Travelers</h4>
@@ -70,8 +83,8 @@ function Searchsection() {
         </Col>
       </Row> 
       <Row>
-        <Col className='apply'lg={2} lg={11}>
-        <Button>Apply</Button>
+        <Col className='apply'lg={11} xs={11}>
+        <Button onClick={apply} >Apply </Button>
         </Col>
       </Row>
       </Modal>
--- src/Components/Signupform.jsx
+++ src/Components/Signupform.jsx
@@ -6,40 +6,51 @@ import Col from 'react-bootstrap/Col';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { Link } from 'react-router-dom';
 import './Signupform.css';
+import { useState } from 'react';
 
 function ContainerFluidExample() {
+    const [validated, setValidated] = useState(false);
+  
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+  
+      setValidated(true);
+    };
   return (
     <>
 
     <Container fluid className='Ccol bg-secondary'>
       <Row>
         <Col className='Logcol bg-light' lg={5}>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit} >
           <h2>SIGN UP</h2>
         <FloatingLabel
         controlId="floatingInput"
         label="Email address"
         className="mb-4 mt-4"
       >
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="email" placeholder="name@example.com" required/>
       </FloatingLabel>
-      <FloatingLabel className=' Pf mb-4 mt-4' controlId="floatingUsername" label="Username">
-        <Form.Control type="text" placeholder="Username" />
+        <FloatingLabel className=' Pf mb-4 mt-4' controlId="floatingUsername" label="Username">
+        <Form.Control type="text" placeholder="Username" required />
       </FloatingLabel>
 
-     
       <FloatingLabel className=' Pf mb-4 mt-4' controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" required/>
       </FloatingLabel>
       <FloatingLabel className=' Pf mb-4 mt-4' controlId="floatingPassword" label="Confirm Password">
-        <Form.Control type="password" placeholder=" Confirm Password" />
+        <Form.Control type="password" placeholder=" Confirm Password" required />
       </FloatingLabel>
 
       <Form.Group className=" ch mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Accept Terms and Conditions" />
+        <Form.Check type="checkbox" label="Accept Terms and Conditions" required />
       </Form.Group>
       <div className="log1">
-      <Button className=' sigbtn bg-primary mb-3' size='lg' type="submit">
+      <Button className=' sigbtn bg-primary mb-3' size='lg' type="submit" >
         Sign Up
       </Button>
       <div className="login">
